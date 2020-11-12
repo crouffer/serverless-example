58b9e8b34e83c46c63e2f39e92f7ee526878ce4f
Christopher J Rouffer <crouffer@pipsc.ca>
2020-11-12 16:26:23 -0500

    IAM permission worked

--- handler.py
+++ handler.py
@@ -1,6 +1,11 @@
 import boto3
+from datetime import datetime
+import hashlib
 import json
 import os
+
+def get_value_from_ssm(env_var:str) -> str:
+    return ssm_client.get_parameter(Name=os.getenv(env_var, None))['Parameter']['Value']
 
 # Service Names
 SERVICE_1='example-service-1',
@@ -8,23 +13,27 @@ SERVICE_2='example-service-2'
 
 
 # Boto Clients
-s3_client = boto3.client('S3')
-sns_client = boto3.client('SNS')
-sqs_client = boto3.client('SQS')
-ssm_client = boto3.client('SSM')
+s3_client = boto3.client('s3')
+sns_client = boto3.client('sns')
+sqs_client = boto3.client('sqs')
+ssm_client = boto3.client('ssm')
 
 
-INBOUND_QUEUE_URL=get_value_from_ssm(env_var='SSM_PARAMETER_S3_QUEUE_URL')
+INBOUND_QUEUE_URL=get_value_from_ssm(env_var='SSM_PARAMETER_INBOUND_QUEUE_URL')
 S3_BUCKET=get_value_from_ssm(env_var='SSM_PARAMETER_S3_BUCKET_NAME')
 SNS_TOPIC_ARN=get_value_from_ssm(env_var='SSM_PARAMETER_SNS_TOPIC_ARN')
 
 def json_to_string(json_object):
     return str(json.dumps(json_object))
 
-def get_value_from_ssm(env_var:str) -> str:
-    return ssm_client.get_parameter(Name=os.getenv(env_var, None))['Value']
+def get_md5sum_of_object(json_object: object) -> str:
+    return hashlib.md5(json.dumps(json_object, sort_keys=True).encode(encoding='UTF-8')).hexdigest()
 
-def write_to_s3(key: str, json_object: object):
+def get_timestamp() -> int:
+    return int(datetime.utcnow().timestamp())
+
+def write_to_s3(service_name: str, json_object: object):
+    key = f"{service_name}/{get_timestamp()}_{get_md5sum_of_object(json_object)}"
     s3_client.put_object(
         Body=json_to_string(json_object),
         Bucket=S3_BUCKET,
@@ -33,7 +42,7 @@ def write_to_s3(key: str, json_object: object):
 def send_to_inbound_queue(service_name: str, json_object: object):
     sqs_client.send_message(
         QueueUrl=INBOUND_QUEUE_URL,
-        MessageBody=json_to_string(json_object))
+        MessageBody=json_to_string(json_object)
     )
 
 def default_transform(event):
@@ -43,10 +52,9 @@ def service_to_sync_helper(service_name: str, event: object, transform_function:
 
     print("BEGIN:  service_to_sync_helper")
     print(f"Processing event from Service: {service_name} - {event}")
-    key = f"{service_name}/{event['Id']}"
 
     # Save the incoming data to S3
-    write_to_s3(key=key, json_object=event)
+    write_to_s3(service_name=service_name, json_object=event)
 
     # Add your data processing here
     output_data=transform_function(event)
@@ -82,4 +90,4 @@ def publish_to_sns(json_object: object) -> None:
     )
 
 def sync_service(event, context):
-    send_to_sns(json_object=event)
+    publish_to_sns(json_object=event)
