0ebed9e47b09769e6ee39b6fea022e3d95ebe360
Christopher J Rouffer <crouffer@pipsc.ca>
2020-11-12 22:52:58 -0500

    Mostly working.  Now just need to add permission for topic to send to queue

--- handler.py
+++ handler.py
@@ -63,7 +63,7 @@ def service_to_sync_helper(service_name: str, event: object, transform_function:
     send_to_inbound_queue(service_name=service_name, json_object=output_data)
     print("END:  service_to_sync_helper")
 
-def from_service_mapper_1(event, context):
+def from_service_1(event, context):
 
     service_to_sync_helper(
         service_name=SERVICE_1,
@@ -73,7 +73,7 @@ def from_service_mapper_1(event, context):
 
     return 200
 
-def from_service_mapper_2(event, context):
+def from_service_2(event, context):
 
     service_to_sync_helper(
         service_name=SERVICE_2,
@@ -83,11 +83,49 @@ def from_service_mapper_2(event, context):
 
     return 200
 
+def sync_to_service_helper(service_name: str, event: object, transform_function: object):
+    print(f"sync_to_service_helper: service_name = {service_name}")
+    print(f"event = {event}")
+    pass
+
+def to_service_1(event, context):
+    sync_to_service_helper(
+        service_name=SERVICE_1,
+        event=event,
+        transform_function=default_transform
+    )
+
+def to_service_2(event, context):
+    sync_to_service_helper(
+        service_name=SERVICE_2,
+        event=event,
+        transform_function=default_transform
+    )
+
 def publish_to_sns(json_object: object) -> None:
-    sns_client.publish(
+    print(f"Sending message to SNS Topic {SNS_TOPIC_ARN}")
+    response = sns_client.publish(
         TopicArn=SNS_TOPIC_ARN,
         Message=json_to_string(json_object)
     )
+    print(f"publish response = {response}")
+
+def delete_message_from_queue(queue_url: str, record: object) -> None:
+    print(f"Deleting message {record['receiptHandle']} from queue {INBOUND_QUEUE_URL}")
+    sqs_client.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=record['receiptHandle']
+    )
 
 def sync_service(event, context):
-    publish_to_sns(json_object=event)
+    print("BEGIN: sync_service")
+    for record in event['Records']:
+        print(f"record = {json.dumps(record)}")
+        
+        # Process the record, then publish the result to sns
+        print(f'CHRIS:  Add the hook here')
+
+        publish_to_sns(json_object=record['body'])
+
+        delete_message_from_queue(queue_url=INBOUND_QUEUE_URL, record=record)
+    print("END: sync_service")
